3f45015c51eb3026f32436779b4c6f0bb691357c
santiesleo <83185169+santiesleo@users.noreply.github.com>
2024-11-27 15:59:14 -0500

    feat: add http request for contracts

--- password.py
+++ password.py
@@ -5,5 +5,5 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-hashed_password = hash_password("hashed_password_012")
+hashed_password = hash_password("hash1234567")
 print(hashed_password)
